f2fcaa190380f43c676082056c708d4d5e92ccda
YuriiHaidai <ura.kiev@gmail.com>
2019-10-12 11:47:15 +0300

    fix hm

---
+++ js/task-04.js
@@ -0,0 +1,22 @@
+"use strict";
+
+let counterValue = 0;
+
+const buttonIncrement = document.querySelector(
+  'button[data-action="increment"]'
+);
+const buttonDecrement = document.querySelector(
+  'button[data-action="decrement"]'
+);
+const value = document.querySelector("#value");
+
+function increment() {
+  // value.innerHTML = counterValue += 1;
+  value.textContent = counterValue += 1;
+}
+function decrement() {
+  value.innerHTML = counterValue -= 1;
+}
+
+buttonIncrement.addEventListener("click", increment);
+buttonDecrement.addEventListener("click", decrement);
